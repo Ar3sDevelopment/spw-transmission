15d6287144f82827054af2959bcc9b2881af6797
Matteo Bruni <ar3shack@gmail.com>
2014-02-18 18:21:23 +0100

    Synced widget values with old DB values

--- transmission.js
+++ transmission.js
@@ -1,6 +1,5 @@
 exports.data = function(cb) {
 	var util = require('util');
-	var exec = require('child_process').exec;
 	var res = {};
 	var Transmission = require('transmission');
 
@@ -75,4 +74,4 @@ exports.manage_post = function(post, cb) {
 
 exports.updatetime = 1000;
 exports.title = 'Transmission';
-exports.columns = 6;
+exports.columns = 4;
